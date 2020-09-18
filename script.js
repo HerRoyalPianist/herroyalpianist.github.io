c2798b2bd9a252111899f3b6a8a000ebe3835796
Morenike Oyewole <herroyalpianist@gmail.com>
2020-09-18 14:46:13 +0100

    hosting website

---
+++ script.js
@@ -0,0 +1,71 @@
+
+const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".slide-in");
+
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -100px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries,appearOnScroll){
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  })
+  
+},appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+})
+
+sliders.forEach(slider => {
+  appearOnScroll.observe(slider);
+  // console.log(slider)
+})
+
+
+ function show(){
+alert ("Thank you for submitting. Your message has been recieved and a reply would be sent shortly.");
+}
+
+// *********************************responsivenavbar*****************************
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+// **************** SLIDESHOW *****************************
+
+const slides = document.querySelectorAll(".mySlides");
+const dots = document.querySelectorAll(".dot");
+// setting the index for the slides
+let slideIndex = 0;
+
+const showSlides = () => {
+    slides.forEach(slide => {
+        slide.style.display = "none";
+    })
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    } 
+    dots.forEach(dot => {
+        dot.className = dot.className.replace("active", "");
+    })
+
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active"; 
+    setTimeout(showSlides, 2000);
+}
+showSlides();
+
